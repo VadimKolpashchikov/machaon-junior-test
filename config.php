79d20bf1f3209dfe0270ac705c19d80e215e9219
VadimKolpashchikov <70956901+VadimKolpashchikov@users.noreply.github.com>
2021-08-19 15:29:14 +0300

    дополняет функцию по критериям ТЗ

--- config.php
+++ config.php
@@ -1,69 +1,45 @@
 <?php
-session_start();
-$option = trim($_POST['option']);
-$default = trim($_POST['default']);
+function throwNewException()
+{
+	throw new Exception('Такого параметра нет.');
+}
+/**
+ * Функция получает путь к файлу settings.php с настройками проекта.
+ * вызывается с 1 или 2 параметрами.
+ * @param string $optionName - строка, необходима для указания ключа опции.
+ * @param mixed $defaultValue - строка, необходима для указания значения опции по умолчанию, если опции в файле нет.
+ * *Если опции нет, и значение по-умолчанию не задано, будет выброшено исключение.
+ * @return mixed
+ */
 
-unset($_SESSION['option']);
-unset($_SESSION['default']);
-unset($_SESSION['result']);
-unset($_SESSION['error']);
-
-$_SESSION['option'] = $option;
-$_SESSION['default'] = $default;
-
-function config($optionName = null, $defaultValue = null) {
+function config(string $optionName = null, $defaultValue = null)
+{
+	error_reporting(E_ALL & ~E_WARNING);
+	$setting = include "data/settings.php";
 	try {
-		$setting = include "data/settings.php";
-		$optionNameConfig = $optionName;
-
-		if(strpos($optionName, ".")) {
+		if (strpos($optionName, ".")) {
 			$optionNameArr = explode(".", $optionName);
 			$arr = $setting;
-
 			foreach ($optionNameArr as $el) {
-				if(array_key_exists($el, $arr)) {
+				if (array_key_exists($el, $arr))
+				{
 					$arr = $arr["$el"];
-				} else if($defaultValue != false) {
-					$arr = $defaultValue;
-					break;
-				} else {
-					throw new Exception('Такого параметра нет.');
 				}
-			}
-			if(is_array($arr)) {
-				ob_start();
-				var_dump($arr);
-				$optionNameConfig = ob_get_contents();
-				ob_end_clean();
-			}else {
-				$optionNameConfig = $arr;
-			}
-		}
-		else {
-			if(array_key_exists($optionNameConfig, $setting)) {
-				if(is_array($setting[$optionName])) {
-					ob_start();
-					var_dump($setting[$optionName]);
-					$optionNameConfig = ob_get_contents();
-					ob_end_clean();
-				}else {
-					$optionNameConfig = $setting[$optionName];
+				else if (isset($defaultValue))
+				{
+					return $defaultValue;
 				}
-			}else if($defaultValue != false) {
-				$optionNameConfig = $defaultValue;
-			}else  {
-				throw new Exception('Такого параметра нет.');
+				else throwNewException();
 			}
-		}
-
-		$_SESSION['result'] =  "Значение: ". $optionNameConfig . "<br>";
-		return $optionNameConfig;
-
-	}catch(Exception $e) {
-		$_SESSION['error'] = 'Выброшено исключение: '.  $e->getMessage();
+			return $arr;
+		} elseif (array_key_exists($optionName, $setting)) {
+			return $setting[$optionName];
+		} elseif (isset($defaultValue)) {
+			return $defaultValue;
+		} else
+			throwNewException();
+	}
+	catch (Exception $e) {
+		echo 'Выброшено исключение: '.  $e->getMessage();
 	}
 }
-
-config($option, $default);
-
-header('Location: index.php');
